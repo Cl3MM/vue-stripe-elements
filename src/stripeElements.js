57c7fec4c1d8793e907785fd3556fe0d340f1fc3
clement <clement@antidots.com>
2018-11-15 12:17:16 +0100

    update: save each instanciated component in the global Stripe.components object

--- src/stripeElements.js
+++ src/stripeElements.js
@@ -1,25 +1,22 @@
 export const Stripe = {
   instance: null,
-  createToken: null,
-  createSource: null,
-  retrieveSource: null,
-  elements: null
+  components: {}
 }
 
 export const baseStyle = {
   base: {
-    color: '#32325d',
-    lineHeight: '24px',
-    fontFamily: 'Helvetica Neue',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
+    color: "#32325d",
+    lineHeight: "24px",
+    fontFamily: "Helvetica Neue",
+    fontSmoothing: "antialiased",
+    fontSize: "16px",
+    "::placeholder": {
+      color: "#aab7c4"
     }
   },
   invalid: {
-    color: '#fa755a',
-    iconColor: '#fa755a'
+    color: "#fa755a",
+    iconColor: "#fa755a"
   }
 }
 
@@ -29,35 +26,42 @@ function init(key, options = {}) {
   }
 
   if (window.Stripe === undefined && Stripe.instance === null) {
-    console.error('Stripe V3 library not loaded!')
+    console.error("Stripe V3 library not loaded!")
   } else if (Stripe.instance === null) {
     Stripe.instance = window.Stripe(key)
   }
 
   if (!Stripe.instance.elements) {
-    console.error('Stripe V3 library not loaded!')
-  } else if (Stripe.elements === null) {
-    Stripe.elements = Stripe.instance.elements(options)
+    console.error("Stripe V3 library not loaded!")
   }
+  //  else if (Stripe.elements === null) {
+  //   Stripe.elements = Stripe.instance.elements(options)
+  // }
 }
 
 export function create(elementType, key_or_stripe, options = {}) {
+  if (Stripe.components.elementType) {
+    console.error(`Stripe element '${elementType}' already defined!`)
+  }
+
   init(key_or_stripe, options.elements || {})
   options.style = Object.assign(baseStyle, options.style || {})
 
-  const element = Stripe.elements.create(elementType, options)
+  const elements = Stripe.instance.elements(options)
+  const element = elements.create(elementType, options)
 
-  Stripe.createToken = (options) => Stripe.instance.createToken(element, options)
-  Stripe.createSource = (options) => Stripe.instance.createSource(element, options)
-  Stripe.retrieveSource = (options) => Stripe.instance.retrieveSource(options)
+  Stripe.components[elementType] = {
+    element,
+    elements,
+    createToken: options => Stripe.instance.createToken(element, options),
+    createSource: options => Stripe.instance.createSource(element, options),
+    retrieveSource: options => Stripe.instance.retrieveSource(options)
+  }
 
   return element
 }
 
 export function destroy() {
-    Stripe.instance = null
-    Stripe.elements = null
-    Stripe.createToken = null
-    Stripe.createSource = null
-    Stripe.retrieveSource = null
+  Stripe.instance = null
+  Stripe.components = {}
 }
