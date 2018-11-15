19db16bb66270c157f83a73fa7d1d1e6775ea2ad
clement <clement@antidots.com>
2018-11-15 12:18:45 +0100

    extracted functionalities in a base mixin

---
+++ src/VStripeElements.vue
@@ -0,0 +1,15 @@
+<script>
+import props from './props'
+import StripeElement from './StripeElement'
+
+export default {
+  props,
+  components: { StripeElement },
+  methods: {
+    blur () { this.$refs.element.blur() },
+    clear () { this.$refs.element.clear() },
+    focus () { this.$refs.element.focus() },
+    update (options) { this.$refs.element.update(options) }
+  }
+}
+</script>
